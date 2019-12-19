baf8f681d018bb5a479806aed7796d8d16792ce5
Zachary Wright <zacharywright77@gmail.com>
2019-12-19 15:26:18 -0600

    implemented include? predicate on the armstrong_numbers class

--- ruby/armstrong-numbers/armstrong_numbers.rb
+++ ruby/armstrong-numbers/armstrong_numbers.rb
@@ -1,5 +1,16 @@
+# frozen_string_literal: true
+
+# This class determines if a given number is an Armstrong Number
 class ArmstrongNumbers
 
+  def self.include?(value)
+    values = value.to_s.chars.map(&:to_i)
+    sum = 0
 
-  
+    values.each do |v|
+      sum += v ** values.length
+    end
+
+    sum == value
+  end
 end
