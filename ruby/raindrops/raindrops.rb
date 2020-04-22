7858d2af9cf75c61c38e33f6607c5565d60cc1df
Zachary Wright <zacharywright77@gmail.com>
2020-04-22 18:32:11 -0500

    Implement Raindrops class
    This implements self.convert which utilizes the concat operator and guard
    clauses for 3, 5, and 7.
    
    Also, this implements a helper function to handle returning an array of
    the factors.

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -1,8 +1,22 @@
-=begin
-Write your code for the 'Raindrops' exercise in this file. Make the tests in
-`raindrops_test.rb` pass.
+# frozen_string_literal: true
 
-To get started with TDD, see the `README.md` file in your
-`ruby/raindrops` directory.
-=end
+class Raindrops
+  def self.convert(num)
+    sound = ''
+    sound += 'Pling' if factors(num).include?(3)
+    sound += 'Plang' if factors(num).include?(5)
+    sound += 'Plong' if factors(num).include?(7)
 
+    return num.to_s if sound.length == 0
+
+    return sound
+  end
+
+  def self.factors(number)
+    [].tap do |arr|
+      (1..number).each do |i|
+        arr.push(i) if number % i == 0
+      end
+    end
+  end
+end
